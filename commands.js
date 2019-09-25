6290555dd0892df0f50523764c220435994919c0
Timothy Brahm <timothy.david.brahm@gmail.com>
2019-09-25 15:10:06 -0400

    adding last three cities

--- commands.js
+++ commands.js
@@ -1,5 +1,3 @@
 
 
-mongoexport -h ds211368.mlab.com:11368 -d heroku_7k8rf6zz -c chicago -u dreamteam -p getgudjawbs4 -o chicago.json
-mongoexport -h ds211368.mlab.com:11368 -d heroku_7k8rf6zz -c newyorkcity -u dreamteam -p getgudjawbs4 -o newyorkcity.json
-mongoexport -h ds211368.mlab.com:11368 -d heroku_7k8rf6zz -c thenewyorkmetropolitanarea -u dreamteam -p getgudjawbs4 -o thenewyorkmetropolitanarea.json
+// mongoexport -h ds211368.mlab.com:11368 -d heroku_7k8rf6zz -c chicago -u dreamteam -p getgudjawbs4 -o chicago.json
