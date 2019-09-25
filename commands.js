261b5a2ced63fddce8b3a8ebc39b301179de7423
Timothy Brahm <timothy.david.brahm@gmail.com>
2019-09-25 15:25:58 -0400

    fixed bad ones

--- commands.js
+++ commands.js
@@ -1,3 +1,3 @@
 
 
-// mongoexport -h ds211368.mlab.com:11368 -d heroku_7k8rf6zz -c chicago -u dreamteam -p getgudjawbs4 -o chicago.json
+// mongoexport -h ds211368.mlab.com:11368 -d heroku_7k8rf6zz -c lowellmasschusetts -u dreamteam -p getgudjawbs4 -o lowellmasschusetts.json
