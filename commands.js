045cf4ce66d5f54afc3661506eb9e23640dbb984
Timothy Brahm <timothy.david.brahm@gmail.com>
2019-09-25 12:26:31 -0400

    most of the rest

--- commands.js
+++ commands.js
@@ -1,3 +1,5 @@
 
 
-mongoexport -h ds211368.mlab.com:11368 -d heroku_7k8rf6zz -c detroit -u dreamteam -p getgudjawbs4 -o detroit.json
+mongoexport -h ds211368.mlab.com:11368 -d heroku_7k8rf6zz -c chicago -u dreamteam -p getgudjawbs4 -o chicago.json
+mongoexport -h ds211368.mlab.com:11368 -d heroku_7k8rf6zz -c newyorkcity -u dreamteam -p getgudjawbs4 -o newyorkcity.json
+mongoexport -h ds211368.mlab.com:11368 -d heroku_7k8rf6zz -c thenewyorkmetropolitanarea -u dreamteam -p getgudjawbs4 -o thenewyorkmetropolitanarea.json
